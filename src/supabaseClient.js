23141140571caf48d8862f62dc31d1e4f2ae00d7
Yuvraj Ab <yuvraajapte@gmail.com>
2026-08-14 21:57:55 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -9,7 +9,7 @@
 // =========================================================
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
+const SUPABASE_URL = "https://uzfwnseiguxdaaqoicrr.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Mr_AaeNYR1YqOobBYMq9pA_6_Hd5Gmf";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
